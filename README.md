2938c82196129d9c8e414a0a11272efde138a335
Jason Mendoza <mernstackdev729@gmail.com>
2024-02-15 14:51:32 -0500

    chore: v2.0

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# EK Longman Chrome Extension
+# Longman Forvo Chrome Extension
+
 Simple, yet handy chrome extension for fans of longman dictionary.
 
 ## How to use
+
 Just type a keyword `longman` before your query in Chrome.
 For example, `longman keep it up`. Then it automatically directs to longman online dictionary with the query, i.e. `https://www.ldoceonline.com/dictionary/english-korean/keep-it-up`
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,31 @@
 const getUrlParam = (url, name) => {
-    name = name.replace(/[\[\]]/g, '\\$&')
-    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-    const results = regex.exec(url)
-    if (!results) return null
-    if (!results[2]) return ''
-    return decodeURIComponent(results[2].replace(/\+/g, ' '))
+  name = name.replace(/[\[\]]/g, "\\$&")
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+  const results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ""
+  return decodeURIComponent(results[2].replace(/\+/g, " "))
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener((tabInfo) => {
-    const tabId = tabInfo.tabId
-    const originUrl = tabInfo.url
-    const isLongmanSearch = /^https:\/\/www\.google\.com\/search\?q=longman\+/
+  const tabId = tabInfo.tabId
+  const originUrl = tabInfo.url
+  const isLongmanSearch = /^https:\/\/www\.google\.com\/search\?q=longman\+/
+  const isForvoSearch = /^https:\/\/www\.google\.com\/search\?q=forvo\+/
 
-    if (isLongmanSearch.test(originUrl)) {
-        const q = getUrlParam(originUrl, 'q')
-        const keyword = q.substring(8).split(' ').join('-')
-        const url = `https://www.ldoceonline.com/dictionary/english-korean/${keyword}`
+  const q = getUrlParam(originUrl, "q")
 
-        chrome.tabs.update(tabId, { url })
-    }
+  if (isLongmanSearch.test(originUrl)) {
+    const keyword = q.substring(8).split(" ").join("-")
+    const url = `https://www.ldoceonline.com/dictionary/english-korean/${keyword}`
+
+    chrome.tabs.update(tabId, { url })
+  }
+
+  if (isForvoSearch.test(originUrl)) {
+    const keyword = q.substring(6)
+    const url = `https://www.forvo.com/search/${keyword}`
+
+    chrome.tabs.update(tabId, { url })
+  }
 })
